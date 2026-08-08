bf6b2a5a9437469db3217ddbd215dfa560ad7d62
zainammar <zainammar74@gmail.com>
2026-08-08 18:05:38 +0500

    bok start

---
+++ js/script-sprinkle.js
@@ -0,0 +1,718 @@
+// New
+function postK1() {
+  var x = document.getElementById("YourextA").value;
+  document.getElementById("yourTextA").innerHTML = x;
+}
+function postK2() {
+  var x = document.getElementById("YourextB").value;
+  document.getElementById("yourTextB").innerHTML = x;
+}
+
+function postK3() {
+  var x = document.getElementById("YourtextC").value;
+  document.getElementById("yourTextC").innerHTML = x;
+}
+
+function postK4() {
+  var x = document.getElementById("YourtextD").value;
+  document.getElementById("yourTextD").innerHTML = x;
+}
+
+function postK5() {
+  var x = document.getElementById("YourtextE").value;
+  document.getElementById("yourTextE").innerHTML = x;
+}
+
+
+
+
+function postK6() {
+  var x = document.getElementById("YourtextF").value;
+  document.getElementById("yourTextF").innerHTML = x;
+}
+
+
+// Update 13 May
+
+// function post15() {
+//   var x = document.getElementById("myTextO").value;
+//   document.getElementById("writeTextO").innerHTML = x;
+// }
+
+
+function postK7() {
+  var x = document.getElementById("XYourtextG").value;
+  document.getElementById("XyourTextG").innerHTML = x;
+}
+
+
+
+function postK8() {
+  var x = document.getElementById("YourtextH").value;
+  document.getElementById("yourTextH").innerHTML = x;
+}
+
+
+
+function postK9() {
+  var x = document.getElementById("XYourtextI").value;
+  document.getElementById("XyourTextI").innerHTML = x;
+}
+
+
+
+
+function postK10() {
+  var x = document.getElementById("YourtextJ").value;
+  document.getElementById("yourTextJ").innerHTML = x;
+}
+
+function postK11() {
+  var x = document.getElementById("YourtextK").value;
+  document.getElementById("yourTextK").innerHTML = x;
+}
+
+
+
+function postK12() {
+  var x = document.getElementById("YourtextL").value;
+  document.getElementById("yourTextL").innerHTML = x;
+}
+
+
+
+function postK13() {
+  var x = document.getElementById("YourtextM").value;
+  document.getElementById("yourTextM").innerHTML = x;
+}
+
+function postK14() {
+  var x = document.getElementById("YourtextN").value;
+  document.getElementById("yourTextN").innerHTML = x;
+}
+
+
+function postK15() {
+  var x = document.getElementById("YourtextO").value;
+  document.getElementById("yourTextO").innerHTML = x;
+}
+
+function postK16() {
+  var x = document.getElementById("YourtextP").value;
+  document.getElementById("yourTextP").innerHTML = x;
+}
+
+
+
+function postK17() {
+  var x = document.getElementById("YourtextQ").value;
+  document.getElementById("yourTextQ").innerHTML = x;
+}
+
+
+function postK18() {
+  var x = document.getElementById("YourtextR").value;
+  document.getElementById("yourTextR").innerHTML = x;
+}
+
+function postK19() {
+  var x = document.getElementById("YourtextS").value;
+  document.getElementById("yourTextS").innerHTML = x;
+}
+
+function postK20() {
+  var x = document.getElementById("YourtextT").value;
+  document.getElementById("yourTextT").innerHTML = x;
+}
+
+function postK21() {
+  var x = document.getElementById("YourtextU").value;
+  document.getElementById("yourTextU").innerHTML = x;
+}
+
+
+function postK22() {
+  var x = document.getElementById("YourtextV").value;
+  document.getElementById("yourTextV").innerHTML = x;
+}
+
+function postK23() {
+  var x = document.getElementById("YourtextW").value;
+  document.getElementById("yourTextW").innerHTML = x;
+}
+
+function postK24() {
+  var x = document.getElementById("YourtextX").value;
+  document.getElementById("yourTextX").innerHTML = x;
+}
+
+function postK25() {
+  var x = document.getElementById("YourtextY").value;
+  document.getElementById("yourTextY").innerHTML = x;
+}
+
+function postK26() {
+  var x = document.getElementById("YourtextZ").value;
+  document.getElementById("yourTextZ").innerHTML = x;
+}
+
+function postK27() {
+  var x = document.getElementById("YourtextAb").value;
+  document.getElementById("yourTextAb").innerHTML = x;
+}
+
+
+function postK28() {
+  var x = document.getElementById("YourtextAc").value;
+  document.getElementById("yourTextAc").innerHTML = x;
+}
+
+
+
+function postK29() {
+  var x = document.getElementById("YourtextAd").value;
+  document.getElementById("yourTextAd").innerHTML = x;
+}
+
+function postK30() {
+  var x = document.getElementById("YourtextAe").value;
+  document.getElementById("yourTextAe").innerHTML = x;
+}
+
+
+function postK31() {
+  var x = document.getElementById("YourtextAf").value;
+  document.getElementById("yourTextAf").innerHTML = x;
+}
+
+function postK32() {
+  var x = document.getElementById("YourtextAg").value;
+  document.getElementById("yourTextAg").innerHTML = x;
+}
+
+function postK33() {
+  var x = document.getElementById("YourtextAh").value;
+  document.getElementById("yourTextAh").innerHTML = x;
+}
+
+
+function postK34() {
+  var x = document.getElementById("YourtextAi").value;
+  document.getElementById("yourTextAi").innerHTML = x;
+}
+
+function postK35() {
+  var x = document.getElementById("YourtextAj").value;
+  document.getElementById("yourTextAj").innerHTML = x;
+}
+
+function postK36() {
+  var x = document.getElementById("YourtextAk").value;
+  document.getElementById("yourTextAk").innerHTML = x;
+}
+
+
+// New end
+function post1() {
+    var x = document.getElementById("myTextA").value;
+    document.getElementById("writeTextA").innerHTML = x;
+  }
+
+
+  // Perfect
+  function post3() {
+    var x = document.getElementById("myTextC").value;
+    document.getElementById("writeTextc").innerHTML = x;
+  }
+
+  // Perfect
+  function post4() {
+    var x = document.getElementById("myTextD").value;
+    document.getElementById("writeTextd").innerHTML = x;
+  }
+
+ 
+  // Perfect
+  function post5() {
+    var x = document.getElementById("myTextE").value;
+    document.getElementById("writeTextE").innerHTML = x;
+  }
+
+
+  function post6() {
+    var x = document.getElementById("myTextF").value;
+    document.getElementById("writeTextF").innerHTML = x;
+  }
+
+  function post7() {
+    var x = document.getElementById("myTextG").value;
+    document.getElementById("writeTextG").innerHTML = x;
+  }
+
+
+
+  function post8() {
+    var x = document.getElementById("myTextH").value;
+    document.getElementById("writeTextH").innerHTML = x;
+  }
+
+
+  function post9() {
+    var x = document.getElementById("myTextI").value;
+    document.getElementById("writeTextI").innerHTML = x;
+  }
+
+  
+  function post10() {
+    var x = document.getElementById("myTextJ").value;
+    document.getElementById("writeTextJ").innerHTML = x;
+  }
+
+  function post11() {
+    var x = document.getElementById("myTextK").value;
+    document.getElementById("writeTextK").innerHTML = x;
+  }
+
+
+  function post12() {
+    var x = document.getElementById("myTextL").value;
+    document.getElementById("writeTextL").innerHTML = x;
+  }
+
+  function post13() {
+    var x = document.getElementById("myTextM").value;
+    document.getElementById("writeTextM").innerHTML = x;
+  }
+
+  function post14() {
+    var x = document.getElementById("myTextN").value;
+    document.getElementById("writeTextN").innerHTML = x;
+  }
+
+  function post15() {
+    var x = document.getElementById("myTextO").value;
+    document.getElementById("writeTextO").innerHTML = x;
+  }
+
+
+
+  function post16() {
+    var x = document.getElementById("myTextB").value;
+    document.getElementById("writeTextB").innerHTML = x;
+  }
+
+
+
+  function post17() {
+    var x = document.getElementById("myTextP").value;
+    document.getElementById("writeTextP").innerHTML = x;
+  }
+
+
+  function post18() {
+    var x = document.getElementById("myTextQ").value;
+    document.getElementById("writeTextQ").innerHTML = x;
+  }
+
+
+
+  function post19() {
+    var x = document.getElementById("myTextR").value;
+    document.getElementById("writeTextR").innerHTML = x;
+  }
+
+
+
+  function post20() {
+    var x = document.getElementById("myTextS").value;
+    document.getElementById("writeTextS").innerHTML = x;
+  }
+
+
+
+  function post21() {
+    var x = document.getElementById("myTextT").value;
+    document.getElementById("writeTextT").innerHTML = x;
+  }
+
+
+
+  function post22() {
+    var x = document.getElementById("myTextU").value;
+    document.getElementById("writeTextU").innerHTML = x;
+  }
+
+
+  function post23() {
+    var x = document.getElementById("myTextV").value;
+    document.getElementById("writeTextV").innerHTML = x;
+  }
+
+
+
+  function post24() {
+    var x = document.getElementById("myTextW").value;
+    document.getElementById("writeTextW").innerHTML = x;
+  }
+
+
+  
+  function post25() {
+    var x = document.getElementById("myTextX").value;
+    document.getElementById("writeTextX").innerHTML = x;
+  }
+
+    
+  function post26() {
+    var x = document.getElementById("myTextY").value;
+    document.getElementById("writeTextY").innerHTML = x;
+  }
+
+    
+  function post27() {
+    var x = document.getElementById("myTextZ").value;
+    document.getElementById("writeTextZ").innerHTML = x;
+  }
+
+// New Sections May 13
+  function posta1() {
+    var x = document.getElementById("myTextA1").value;
+    document.getElementById("writeTextA1").innerHTML = x;
+  }
+
+
+  function posta2() {
+    var x = document.getElementById("myTextA2").value;
+    document.getElementById("writeTextA2").innerHTML = x;
+  }
+
+
+  function posta3() {
+    var x = document.getElementById("myTextA3").value;
+    document.getElementById("writeTextA3").innerHTML = x;
+  }
+
+  function posta4() {
+    var x = document.getElementById("myTextA4").value;
+    document.getElementById("writeTextA4").innerHTML = x;
+  }
+
+  function posta5() {
+    var x = document.getElementById("myTextA5").value;
+    document.getElementById("writeTextA5").innerHTML = x;
+  }
+
+  function posta6() {
+    var x = document.getElementById("myTextA6").value;
+    document.getElementById("writeTextA6").innerHTML = x;
+  }
+
+  function posta7() {
+    var x = document.getElementById("myTextA7").value;
+    document.getElementById("writeTextA7").innerHTML = x;
+  }
+
+  function posta8() {
+    var x = document.getElementById("myTextA8").value;
+    document.getElementById("writeTextA8").innerHTML = x;
+  }
+
+  function posta9() {
+    var x = document.getElementById("myTextA9").value;
+    document.getElementById("writeTextA9").innerHTML = x;
+  }
+
+// post-1 style - start
+  function posta10() {
+    var x = document.getElementById("myTextA10").value;
+    document.getElementById("writeTextA10").innerHTML = x;
+  }
+
+  function posta11() {
+    var x = document.getElementById("myTextA11").value;
+    document.getElementById("writeTextA11").innerHTML = x;
+  }
+
+
+  function posta12() {
+    var x = document.getElementById("myTextA12").value;
+    document.getElementById("writeTextA12").innerHTML = x;
+  }
+
+
+  function posta13() {
+    var x = document.getElementById("myTextA13").value;
+    document.getElementById("writeTextA13").innerHTML = x;
+  }
+
+
+  function posta14() {
+    var x = document.getElementById("myTextA14").value;
+    document.getElementById("writeTextA14").innerHTML = x;
+  }
+
+
+  function posta15() {
+    var x = document.getElementById("myTextA15").value;
+    document.getElementById("writeTextA15").innerHTML = x;
+  }
+
+
+  function posta16() {
+    var x = document.getElementById("myTextA16").value;
+    document.getElementById("writeTextA16").innerHTML = x;
+  }
+
+
+
+  function posta16() {
+    const text = document.getElementById("myTextA16").value;
+    const color = document.getElementById("textColor").value;
+    const output = document.getElementById("writeTextA16");
+
+    output.innerHTML = text;
+    output.style.color = color; // change text color
+  }
+
+
+  
+  function posta17() {
+    var x = document.getElementById("myTextA17").value;
+    document.getElementById("writeTextA17").innerHTML = x;
+  }
+
+
+  function posta18() {
+    var x = document.getElementById("myTextA18").value;
+    document.getElementById("writeTextA18").innerHTML = x;
+  }
+
+
+  function ina1() {
+    var x = document.getElementById("myina1").value;
+    document.getElementById("writeina1").innerHTML = x;
+  }
+
+
+
+  function changeBackground() {
+    const color = document.getElementById('colorPicker').value;
+    document.getElementById('bgdark').style.backgroundColor = color;
+  }
+
+
+
+  function changeBackgroundA() {
+    const color = document.getElementById('colorPickerA').value;
+    document.getElementById('bgdark1').style.backgroundColor = color;
+  }
+
+  function changeBackgroundB() {
+    const color = document.getElementById('colorPickerB').value;
+    document.getElementById('bgdark2').style.backgroundColor = color;
+  }
+
+
+
+  function changeBackgroundC() {
+    const color = document.getElementById('colorPickerC').value;
+    document.getElementById('bgdark3').style.backgroundColor = color;
+  }
+
+
+  
+  function changeBackgroundD() {
+    const color = document.getElementById('colorPickerD').value;
+    document.getElementById('bgdark4').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundE() {
+    const color = document.getElementById('colorPickerE').value;
+    document.getElementById('bgdark5').style.backgroundColor = color;
+  }
+
+  function changeBackgroundF() {
+    const color = document.getElementById('colorPickerF').value;
+    document.getElementById('bgdark6').style.backgroundColor = color;
+  }
+
+  function changeBackgroundG() {
+    const color = document.getElementById('colorPickerG').value;
+    document.getElementById('bgdark7').style.backgroundColor = color;
+  }
+
+  function changeBackgroundH() {
+    const color = document.getElementById('colorPickerH').value;
+    document.getElementById('bgdark8').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundI() {
+    const color = document.getElementById('colorPickerI').value;
+    document.getElementById('bgdark9').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundJ() {
+    const color = document.getElementById('colorPickerJ').value;
+    document.getElementById('bgdark10').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundK() {
+    const color = document.getElementById('colorPickerK').value;
+    document.getElementById('bgdark11').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundL() {
+    const color = document.getElementById('colorPickerL').value;
+    document.getElementById('bgdark12').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundM() {
+    const color = document.getElementById('colorPickerM').value;
+    document.getElementById('bgdark13').style.backgroundColor = color;
+  }
+
+  function changeBackgroundN() {
+    const color = document.getElementById('colorPickerN').value;
+    document.getElementById('bgdark14').style.backgroundColor = color;
+  }
+
+  function changeBackgroundO() {
+    const color = document.getElementById('colorPickerO').value;
+    document.getElementById('bgdark15').style.backgroundColor = color;
+  }
+
+  function changeBackgroundP() {
+    const color = document.getElementById('colorPickerP').value;
+    document.getElementById('bgdark16').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundQ() {
+    const color = document.getElementById('colorPickerQ').value;
+    document.getElementById('bgdark17').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundR() {
+    const color = document.getElementById('colorPickerR').value;
+    document.getElementById('bgdark18').style.backgroundColor = color;
+  }
+
+
+
+  function changeBackgroundS() {
+    const color = document.getElementById('colorPickerS').value;
+    document.getElementById('bgdark19').style.backgroundColor = color;
+  }
+
+  function changeBackgroundT() {
+    const color = document.getElementById('colorPickerT').value;
+    document.getElementById('bgdark20').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundU() {
+    const color = document.getElementById('colorPickerU').value;
+    document.getElementById('bgdark21').style.backgroundColor = color;
+  }
+
+  function changeBackgroundV() {
+    const color = document.getElementById('colorPickerV').value;
+    document.getElementById('bgdark22').style.backgroundColor = color;
+  }
+
+  function changeBackgroundW() {
+    const color = document.getElementById('colorPickerW').value;
+    document.getElementById('bgdark23').style.backgroundColor = color;
+  }
+
+
+
+  function changeBackgroundX() {
+    const color = document.getElementById('colorPickerX').value;
+    document.getElementById('bgdark24').style.backgroundColor = color;
+  }
+
+  function changeBackgroundY() {
+    const color = document.getElementById('colorPickerY').value;
+    document.getElementById('bgdark25').style.backgroundColor = color;
+  }
+
+  function changeBackgroundZ() {
+    const color = document.getElementById('colorPickerZ').value;
+    document.getElementById('bgdark26').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundAa() {
+    const color = document.getElementById('colorPickerAa').value;
+    document.getElementById('bgdark27').style.backgroundColor = color;
+  }
+
+  function changeBackgroundAb() {
+    const color = document.getElementById('colorPickerAb').value;
+    document.getElementById('bgdark28').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundAc() {
+    const color = document.getElementById('colorPickerAc').value;
+    document.getElementById('bgdark29').style.backgroundColor = color;
+  }
+
+  function changeBackgroundAd() {
+    const color = document.getElementById('colorPickerAd').value;
+    document.getElementById('bgdark30').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundAe() {
+    const color = document.getElementById('colorPickerAe').value;
+    document.getElementById('bgdark31').style.backgroundColor = color;
+  }
+
+
+  function changeBackgroundAf() {
+    const color = document.getElementById('colorPickerAf').value;
+    document.getElementById('bgdark32').style.backgroundColor = color;
+  }
+
+
+
+  function changeBackgroundAg() {
+    const color = document.getElementById('colorPickerAg').value;
+    document.getElementById('bgdark33').style.backgroundColor = color;
+  }
+
+
+
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+    // You can change the file names here
+    const filesToLoad = {
+      content1: 'page1.html',
+      content2: 'page2.html'
+    };
+  
+    // Loop through and load each file into its matching div
+    for (const [divId, filename] of Object.entries(filesToLoad)) {
+      fetch(filename)
+        .then(response => {
+          if (!response.ok) throw new Error(`File not found: ${filename}`);
+          return response.text();
+        })
+        .then(data => {
+          document.getElementById(divId).innerHTML = data;
+        })
+        .catch(error => {
+          document.getElementById(divId).innerHTML = `<p style="color:red">${error.message}</p>`;
+        });
+    }
+  });
